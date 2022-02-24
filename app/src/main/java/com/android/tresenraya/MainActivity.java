ecf5a4902b8ade9ec3fea81ad1a806cf05bff296
martifarre <92933891+martifarre@users.noreply.github.com>
2022-02-24 19:06:21 +0100

    created first button

--- app/src/main/java/com/android/tresenraya/MainActivity.java
+++ app/src/main/java/com/android/tresenraya/MainActivity.java
@@ -3,12 +3,28 @@ package com.android.tresenraya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int turno;
+
+    private Button btn_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_1 = findViewById(R.id.btn_1);
+
+        btn_1.setOnClickListener(view -> verifyWin(true, true));
+
     }
+
+
+    private void verifyWin(boolean answer, boolean isTrue) {
+
+    }
+
 }
